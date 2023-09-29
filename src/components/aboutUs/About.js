8ccdac64c5b5ce39bc154af685d6e219e90aa348
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-09-30 00:16:43 +0200

    atMedia About

--- src/components/aboutUs/About.js
+++ src/components/aboutUs/About.js
@@ -15,10 +15,11 @@ const About = () => {
                     <h2 className="about-subtitle">Cozy Home is with you along the way.</h2>
                     <p className="about-paragraph">Born in the heart of brussels, Cozy Home helps you find the perfect accomodation for you. You will be able to discover houses , appartaments , cabins and much more. </p>
                 </div>
-                <div className="Gallery">
-                    <PhotoGallery />
-                </div>
+                {/* <div className="Gallery"> */}
+
+                {/* </div> */}
             </div>
+            <PhotoGallery />
         </main>
     )
 }
--- src/components/photogallery/PhotoGallery.js
+++ src/components/photogallery/PhotoGallery.js
@@ -18,10 +18,10 @@ const PhotoGallery = () => {
 
 
     const images = [
-        { src: 'https://images.unsplash.com/photo-1597667554307-885851e705c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80', alt: 'Image 1' },
-        { src: 'https://images.unsplash.com/photo-1596510915124-38eaa5517966?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80', alt: 'Image 2' },
-        { src: 'https://plus.unsplash.com/premium_photo-1663106412202-22ae183bfbd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1504&q=80', alt: 'Image 3' },
-        { src: 'https://images.unsplash.com/photo-1609535883413-0078310a6a6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80', alt: 'Image 4' },
+        { src: 'https://images.unsplash.com/photo-1597667554307-885851e705c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80', alt: 'Couple' },
+        { src: 'https://images.unsplash.com/photo-1596510915124-38eaa5517966?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80', alt: 'Family' },
+        { src: 'https://plus.unsplash.com/premium_photo-1663106412202-22ae183bfbd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1504&q=80', alt: 'Friends-Group' },
+        { src: 'https://images.unsplash.com/photo-1615789885218-5d9292457126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80', alt: 'Little-Girl' },
     ];
 
 
