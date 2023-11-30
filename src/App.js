e6753bcf0006f669ccf4bfe359c86b17089e9301
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-11-30 19:22:33 +0100

    added lazy/suspense for property/properties

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from "react"
+// import React from "react"
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 
 import Homepage from "./components/homepage/Homepage"
-import Navigation from "./components/navigation/Navigation"
-import Footer from "./components/footer/Footer"
 import Register from "./components/register/Register"
 import About from "./components/aboutUs/About"
 import Login from "./components/signIn/Login"
@@ -13,31 +12,32 @@ import Propertylist from "./components/propertylist/PropertyList"
 import Idproperty from "./components/idproperty/IdProperty"
 
 
+const Navigation = lazy(() => import("./components/navigation/Navigation"));
+const Footer = lazy(() => import("./components/footer/Footer"));
 
 const App = () => {
   return (
-
     <main style={{ 'height': '100vh' }}>
-
-
       <Router>
-        {<Navigation />}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/property/properties" element={<Propertylist />} />
+            <Route path="/property/:properyid" element={<Idproperty />} />
+            <Route path="/admin" element={<Adminspace />} />
+          </Routes>
 
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/property/properties" element={<Propertylist />} />
-          <Route path="/property/:properyid" element={<Idproperty />} />
-          <Route path="/admin" element={<Adminspace />} />
-
-        </Routes>
-
-        {<Footer />}
+          <Footer />
+        </Suspense>
       </Router>
     </main>
   );
-}
+};
 
 export default App;
+
+
